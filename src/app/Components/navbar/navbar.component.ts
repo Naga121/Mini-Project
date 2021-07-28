2eb97f6613200f61a42a2334c8170717ca653a23
github.naga@gmail.com <GitHub.naga@gmail.com.com>
2021-07-28 07:44:46 +0530

    master

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  logo=['assets/logo.png']
+  
   constructor() { }
 
   ngOnInit(): void {
